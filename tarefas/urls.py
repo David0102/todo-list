79e2378f17fdf85012a01532c8dd812f53587624
David0102 <davidmoura0102@gmail.com>
2024-09-24 12:30:24 -0300

    finalizando vulnerabilidades sqli

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,6 +3,6 @@ from tarefas import views
 
 urlpatterns = [
     path('tarefas/create/', views.criar_tarefa, name='tarefas_create'),
-    path('tarefas/update_status/<int:id>', views.concluir_tarefa, name='tarefas_update_status'),
-    path('tarefas/delete/<int:id>', views.remover_tarefa, name='tarefas_delete'),
+    path('tarefas/update_status/<str:id>', views.concluir_tarefa, name='tarefas_update_status'),
+    path('tarefas/delete/<str:id>', views.remover_tarefa, name='tarefas_delete'),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from tarefas.models import Tarefa
 from user.models import Usuario
 from datetime import datetime
@@ -21,8 +21,8 @@ def criar_tarefa(request):
                data_atual = datetime.now().date()
                user_id = user.id
 
-               query = f"INSERT INTO tarefas (titulo, descricao, data_criacao, status, user_id) VALUES ('{titulo}', '{descricao}', '{data_atual}', 'pendente', '{user_id}');"
-
+               query = f"INSERT INTO tarefas (data_criacao, status, user_id, titulo, descricao) VALUES ('{data_atual}', 'pendente', '{user_id}', '{titulo}', '{descricao}');"
+               print(query)
                with connection.cursor() as cursor:
                   cursor.execute(query)
                
@@ -34,19 +34,6 @@ def criar_tarefa(request):
             return redirect('login')
       except Usuario.DoesNotExist:
          return redirect('login')
-      
-#Simula a função get_object_or_404 do django. Mas nesse caso, foi utilizado consulta sql pura, sem orm.
-def obter_tarefa_ou_404(tarefa_id, user_id):
-    query = "SELECT * FROM tarefas WHERE id = %s AND user_id = %s"
-    
-    with connection.cursor() as cursor:
-        cursor.execute(query, [tarefa_id, user_id])
-        tarefa = cursor.fetchone()
-
-    if not tarefa:
-        raise Http404("Tarefa não encontrada")
-    
-    return tarefa
 
 def concluir_tarefa(request, id):
    if 'username' in request.session:
@@ -56,15 +43,17 @@ def concluir_tarefa(request, id):
          user = Usuario.objects.get(username=username)
 
          if user.is_authenticated:
-            tarefa = obter_tarefa_ou_404(id, user.id)
-
             query = f"UPDATE tarefas SET status = 'concluida' WHERE id = '{id}'"
 
             with connection.cursor() as cursor:
                cursor.execute(query)
-            
-            messages.success(request, 'Tarefa marca como concluída.')
-            return redirect('index')
+               rows_affected = cursor.rowcount
+
+            if rows_affected> 0:
+               messages.success(request, 'Tarefa marcada como concluída.')
+               return redirect('index')
+            else:
+               raise Http404('Tarefa não encontrada')
          else:
             return redirect('login')
       except Usuario.DoesNotExist:
@@ -79,8 +68,6 @@ def remover_tarefa(request, id):
          user = Usuario.objects.get(username=username)
 
          if user.is_authenticated:
-            tarefa = obter_tarefa_ou_404(id, user.id)
-
             query = f"DELETE FROM tarefas WHERE id = '{id}'"
 
             with connection.cursor() as cursor:
