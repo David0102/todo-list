a0c185131d1dadc90797e24a52afd65653e2647c
David0102 <davidmoura0102@gmail.com>
2024-09-24 09:38:08 -0300

    criaçã de função para obter um tarefa por id sem uso de orm

--- tarefas/views.py
+++ tarefas/views.py
@@ -4,6 +4,7 @@ from user.models import Usuario
 from datetime import datetime
 from django.db import connection
 from django.contrib import messages
+from django.http import Http404
 
 
 def criar_tarefa(request):
@@ -33,6 +34,19 @@ def criar_tarefa(request):
             return redirect('login')
       except Usuario.DoesNotExist:
          return redirect('login')
+      
+#Simula a função get_object_or_404 do django. Mas nesse caso, foi utilizado consulta sql pura, sem orm.
+def obter_tarefa_ou_404(tarefa_id, user_id):
+    query = "SELECT * FROM tarefas WHERE id = %s AND user_id = %s"
+    
+    with connection.cursor() as cursor:
+        cursor.execute(query, [tarefa_id, user_id])
+        tarefa = cursor.fetchone()
+
+    if not tarefa:
+        raise Http404("Tarefa não encontrada")
+    
+    return tarefa
 
 def concluir_tarefa(request, id):
    if 'username' in request.session:
@@ -42,6 +56,8 @@ def concluir_tarefa(request, id):
          user = Usuario.objects.get(username=username)
 
          if user.is_authenticated:
+            tarefa = obter_tarefa_ou_404(id, user.id)
+
             query = f"UPDATE tarefas SET status = 'concluida' WHERE id = '{id}'"
 
             with connection.cursor() as cursor:
@@ -54,6 +70,7 @@ def concluir_tarefa(request, id):
       except Usuario.DoesNotExist:
          return redirect('login')
 
+
 def remover_tarefa(request, id):
    if 'username' in request.session:
       username = request.session['username']
@@ -62,7 +79,7 @@ def remover_tarefa(request, id):
          user = Usuario.objects.get(username=username)
 
          if user.is_authenticated:
-            tarefa = get_object_or_404(Tarefa, id=id, user=user)
+            tarefa = obter_tarefa_ou_404(id, user.id)
 
             query = f"DELETE FROM tarefas WHERE id = '{id}'"
 
